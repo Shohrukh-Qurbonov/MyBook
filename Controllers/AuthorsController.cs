18d0f7ee965d8e896eeb96194d01933ad496efe8
Shohrukh <qurbonov.shohrukh@mail.ru>
2021-10-15 01:37:32 +0500

    Добавьте файлы проекта.

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyBook.Data.Services;
+using MyBook.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBook.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private AuthorsService _authorsService;
+        public AuthorsController(AuthorsService authorsService)
+        {
+            _authorsService = authorsService;
+        }
+
+        [HttpPost("add-author")]
+        public IActionResult AddAuthor([FromBody] AuthorVM author)
+        {
+            _authorsService.AddAuthor(author);
+            return Ok();
+        }
+    }
+}
---
+++ Data/Services/AuthorsService.cs
@@ -0,0 +1,28 @@
+﻿using MyBook.Data.Models;
+using MyBook.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBook.Data.Services
+{
+    public class AuthorsService
+    {
+        private AppDbContext _context;
+        public AuthorsService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddAuthor(AuthorVM author)
+        {
+            var _author = new Author()
+            {
+                FullName = author.FullName
+            };
+            _context.Authors.Add(_author);
+            _context.SaveChanges();
+        }
+    }
+}
